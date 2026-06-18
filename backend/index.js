ac41a5419291217ebbaa03d198229993a41b828d
Devagoni Sai Kumar Goud <venkateshdevagoni123@gmail.com>
2026-06-18 15:32:29 +0530

    Add PATCH to CORS allowed methods

--- backend/index.js
+++ backend/index.js
@@ -140,7 +140,7 @@ app.use(cors({
     'https://lb-nagar-market.vercel.app'
   ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
